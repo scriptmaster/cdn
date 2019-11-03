271c30f58bceaef623bd32915342d85932905718
ScriptMaster <msheriffusa@gmail.com>
2019-11-03 11:33:03 +0530

    Update pattern-2.js

--- js/pattern-2.js
+++ js/pattern-2.js
@@ -3,8 +3,20 @@
   let last_known_scroll_position = 0;
   let ticking = false;
 
+  var scrollOffsetpx = 30;
+  var scrollToPxRatio = 1;
+  var maxScrollPx = scrollOffsetpx+(100*scrollToPxRatio);
+
   function updateHeaderNavByScroll(scroll_pos) {
     console.log(scroll_pos);
+    var op=0;
+    if(scroll_pos > 30)
+    {
+      if(scroll_pos >= maxScrollPx)
+        op=1;
+      else op=(scroll_pos-scrollOffsetpx) / (maxScrollPx-scrollOffsetpx);
+    }
+    console.log(op);
   }
 
   window.addEventListener('scroll', function(e) {
