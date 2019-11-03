67bcf5b9fc14dea63fec0155c40079c378716df4
ScriptMaster <msheriffusa@gmail.com>
2019-11-03 11:09:59 +0530

    Create pattern-2.js

---
+++ js/pattern-2.js
@@ -0,0 +1,22 @@
+(function(){
+
+  let last_known_scroll_position = 0;
+  let ticking = false;
+
+  function updateHeaderNavByScroll(scroll_pos) {
+    console.log(scroll_pos);
+  }
+
+  window.addEventListener('scroll', function(e) {
+    last_known_scroll_position = window.scrollY;
+    ticking = true;
+    if (!ticking) {
+      window.requestAnimationFrame(function() {
+        updateHeaderNavByScroll(last_known_scroll_position);
+        ticking = false;
+      });
+    }
+  });
+
+
+})();
