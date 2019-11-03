3b4482f07e84166632619a45092381427508eb04
ScriptMaster <msheriffusa@gmail.com>
2019-11-03 11:50:46 +0530

    Update pattern-2.js

--- js/pattern-2.js
+++ js/pattern-2.js
@@ -5,12 +5,11 @@
 
   var scrollNavColor = '0,0,0';
   var scrollOffsetpx = 30;
-  var scrollToPxRatio = 1;
+  var scrollToPxRatio = 3;
   var maxScrollPx = scrollOffsetpx+(100*scrollToPxRatio);
   var previousScrollOp = -1;
 
   function updateHeaderNavByScroll(scroll_pos) {
-    console.log(scroll_pos);
     var op=0;
     if(scroll_pos > 30) {
       if(scroll_pos >= maxScrollPx)
@@ -18,10 +17,9 @@
       else op=(scroll_pos-scrollOffsetpx) / (maxScrollPx-scrollOffsetpx);
     }
     op = Math.round(op*100,0)/100;
-    console.log(op);
     if(previousScrollOp!=op) {
       previousScrollOp=op;
-      console.log('Updating opacituad');
+      console.log('Updating opacituad', op);
       document.getElementsByTagName('nav')[0].style.backgroundColor='rgba('+scrollNavColor+','+op+')';
     }
   }
