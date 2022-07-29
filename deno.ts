452a31458c38f053fbbb618452b06828b42d7801
ScriptMaster <scriptmaster@gmail.com>
2022-07-30 00:51:42 +0530

    embed first paint

--- deno.ts
+++ deno.ts
@@ -29,6 +29,8 @@ async function handleRequest(request: Request): Promise<Response> {
     `<!doctype html>
 <html>
 <head>
+    <title>CDN</title>
+    <style type="text/css">/*Embed First paint*/ html { font: 16px Verdana, Roboto, Garamond, serif; box-sizing: border-box; } body { background: radial-gradient(circle at 50% 50%, rgba(47,51,140,0.8), rgba(0,0,0,0.8)), url("data:image/svg+xml,%3Csvg viewBox='0 0 800 800' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.8' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E"); } ol, ul { list-style: none; } body,h1,h2,h3,h4,h5,h6,p,ol,ul { margin: 0; padding: 0; font-weight: normal; } *, *:before, *:after { box-sizing: inherit; }</style>
     <link rel="stylesheet" href="/css/difficulture.css" />
 </head>
 <body>
